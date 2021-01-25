0db6a3590968459c62879fcc301ee586b7427b90
Fabian Conejo <fabianconejosegu@gmail.com>
2021-01-25 12:01:39 -0600

    New goUp function in javascript jquery to facilitate going back home without scrollling.

--- assets/js/index.js
+++ assets/js/index.js
@@ -16,4 +16,22 @@ $(document).ready(function(){
     })
   })
 
+//function to go up conveniently with a little span positioned to the bottom-right   
+
+  $(document).ready(function(){
+
+    $('.go-up').click(function(){
+      $('body, html').animate({
+        scrollTop: '0px'
+      }, 300);
+    });
   
+    $(window).scroll(function(){
+      if( $(this).scrollTop() > 0 ){
+        $('.go-up').slideDown(300);
+      } else {
+        $('.go-up').slideUp(300);
+      }
+    });
+  
+  });
